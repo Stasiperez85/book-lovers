5fd45d4359711907612a4a7fc3bf94dd941ab637
Anastasia Perez <Stasiperez85@gmail.com>
2022-05-05 21:34:13 -0600

    update typedefs and resolvers and solve code error

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+type Query {
+ me: User
+}
+type User {
+  _id: ID!
+  username: String
+  email: String
+  bookCount: Int
+  savedBooks: [Book]
+}
+type Book {
+  bookId: ID!
+  authors: [String]
+  description: String
+  title: String
+  image: String
+  link: String
+}
+type Auth {
+  token: ID!
+  user: User
+}
+input BookInput {
+  bookId: ID!
+  authors: [String]
+  description: String
+  title: String
+  image: String
+  link: String
+}
+type Mutation {
+  login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+}
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
