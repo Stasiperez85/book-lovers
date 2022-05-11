af11df2258af90338a5ec5c7242cbb405b122040
Anastasia Perez <Stasiperez85@gmail.com>
2022-05-10 20:44:17 -0600

    completed typedefs and resolvers and add front end

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
+const { authMiddleware } = require('./utils/auth');
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -13,7 +14,8 @@ const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  // context: authMiddleware
 });
 
 const app = express();
