6e3339aa414b848186f01fc88680ddc62f696f35
Anastasia Perez <Stasiperez85@gmail.com>
2022-05-13 21:20:26 -0600

    updated savedbooks.js to remove books

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -28,7 +28,7 @@ const SavedBooks = () => {
     }
 
     try {
-      const { response } = await removeBook({ variables: { bookId } });
+      const { response } = await removeBook({ variables: bookId });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
